d1aa13c5e1fae32117e7d5a917d1da22801b9901
Kiril Videlov <kiril.vi@runbox.no>
2019-12-04 18:52:06 +0100

    Add day 3 solutions

--- day_3/day_3.go
+++ day_3/day_3.go
@@ -3,123 +3,108 @@ package day_3
 import (
 	"advent-of-code-2019/input"
 	"fmt"
+	"github.com/emirpasic/gods/sets"
+	"github.com/emirpasic/gods/sets/linkedhashset"
 	"math"
 	"strconv"
 	"strings"
 )
 
-func Part1() {
+func Part2() {
 	lines := input.Lines("day_3/in.txt")
+	firstWire, firstWireDists := lineCoords(toInstArray(lines[0]))
+	secondWire, secondWireDists := lineCoords(toInstArray(lines[1]))
+	intersections := setIntersection(firstWire, secondWire)
 
-	first := toInstArray(lines[0])
-	second := toInstArray(lines[1])
-
-	//fmt.Println(first)
-	//fmt.Println(second)
-
-	grid := initialize(20000)
-
-	x := len(grid) / 2
-	y := len(grid) / 2
-
-	grid[y][x] = 1
-
-	for _, in := range first {
-		sx := x
-		sy := y
-		if in.Dir == "R" {
-			x = x + in.Magn
-		} else if in.Dir == "L" {
-			x = x - in.Magn
-		} else if in.Dir == "U" {
-			y = y - in.Magn
-		} else if in.Dir == "D" {
-			y = y + in.Magn
+	minDist := math.MaxFloat64
+	for _, x := range intersections.Values() {
+		coords := x.(pos)
+		if coords.X == 0 && coords.Y == 0 {
+			continue
 		}
-		flagBetween(grid, sx, sy, x, y, 1)
-	}
-
-	x = len(grid) / 2
-	y = len(grid) / 2
-
-	for _, in := range second {
-		sx := x
-		sy := y
-		if in.Dir == "R" {
-			x = x + in.Magn
-		} else if in.Dir == "L" {
-			x = x - in.Magn
-		} else if in.Dir == "U" {
-			y = y - in.Magn
-		} else if in.Dir == "D" {
-			y = y + in.Magn
-		}
-		flagBetween(grid, sx, sy, x, y, 5)
-	}
-
-	//for i := 0; i< len(grid); i++ {
-	//	fmt.Println(grid[i])
-	//}
-	minDist := 100000.0
-	for _, coords := range findInter(grid) {
-		d := manhattanDistance(len(grid)/2, len(grid)/2, coords[1], coords[0])
-		if d < minDist {
-			minDist = d
-		}
+		minDist = math.Min(minDist,
+			float64(firstWireDists[coords] + secondWireDists[coords]))
 	}
 	fmt.Println(minDist)
 }
 
-func manhattanDistance(fromX, fromY, toX, toY int) float64 {
-	return math.Abs(float64(toX-fromX)) + math.Abs(float64(toY-fromY))
+func Part1() {
+	lines := input.Lines("day_3/in.txt")
+	firstWire, _ := lineCoords(toInstArray(lines[0]))
+	secondWire, _ := lineCoords(toInstArray(lines[1]))
+	intersections := setIntersection(firstWire, secondWire)
+
+	minDist := math.MaxFloat64
+	for _, x := range intersections.Values() {
+		coords := x.(pos)
+		if coords.X == 0 && coords.Y == 0 {
+			continue
+		}
+		minDist = math.Min(minDist,
+			math.Abs(float64(coords.X)) + math.Abs(float64(coords.Y)))
+	}
+	fmt.Println(minDist)
 }
 
-func findInter(grid [][]int) [][]int {
-	out := [][]int{}
-	for y := 0; y < len(grid); y++ {
-		for x := 0; x < len(grid); x++ {
-			if grid[y][x] == 6 {
-				out = append(out, []int{y, x})
+func setIntersection(a, b sets.Set) sets.Set {
+	out := linkedhashset.New()
+	for _, x := range a.Values() {
+		if b.Contains(x) {
+			out.Add(x)
+		}
+	}
+	return out
+}
+
+func lineCoords(in []instr) (sets.Set, map[pos]int) {
+	s := linkedhashset.New()
+	s.Add(pos{0, 0})
+	current := pos{Y:0, X:0}
+	totalDist := 0
+	distToPoint := map[pos]int{}
+	for _, i := range in {
+		if i.Dir == "R" {
+			for x := 1; x <= i.Magn; x++ {
+				totalDist++
+				p := pos{current.Y, current.X + x}
+				s.Add(p)
+				distToPoint[p] = totalDist
 			}
+			current = s.Values()[s.Size()-1].(pos)
+			continue
+		}
+		if i.Dir == "L" {
+			for x := 1; x <= i.Magn; x++ {
+				totalDist++
+				p := pos{current.Y, current.X - x}
+				s.Add(p)
+				distToPoint[p] = totalDist
+			}
+			current = s.Values()[s.Size()-1].(pos)
+			continue
+		}
+		if i.Dir == "U" {
+			for x := 1; x <= i.Magn; x++ {
+				totalDist++
+				p := pos{current.Y + x, current.X}
+				s.Add(p)
+				distToPoint[p] = totalDist
+			}
+			current = s.Values()[s.Size()-1].(pos)
+			continue
+		}
+		if i.Dir == "D" {
+			for x := 1; x <= i.Magn; x++ {
+				totalDist++
+				p := pos{current.Y - x, current.X}
+				s.Add(p)
+				distToPoint[p] = totalDist
+			}
+			current = s.Values()[s.Size()-1].(pos)
+			continue
 		}
 	}
-	return out
-}
-
-func Part2() {
-	//lines := input.Lines("day_3/in.txt")
-	//fmt.Println(lines)
-}
-
-func flagBetween(grid [][]int, sX, sY, eX, eY, amnt int) {
-	if sX < eX {
-		for i := sX + 1; i <= eX; i++ {
-			grid[sY][i] = grid[sY][i] + amnt
-		}
-	}
-	if sX > eX {
-		for i := sX - 1; i >= eX; i-- {
-			grid[sY][i] = grid[sY][i] + amnt
-		}
-	}
-	if sY < eY {
-		for i := sY + 1; i <= eY; i++ {
-			grid[i][sX] = grid[i][sX] + amnt
-		}
-	}
-	if sY > eY {
-		for i := sY - 1; i >= eY; i-- {
-			grid[i][sX] = grid[i][sX] + amnt
-		}
-	}
-}
-
-func initialize(dim int) [][]int {
-	out := [][]int{}
-	for i := 0; i < dim; i++ {
-		out = append(out, make([]int, dim))
-	}
-	return out
+	return s, distToPoint
 }
 
 func toInstArray(line string) []instr {
@@ -134,6 +119,11 @@ func toInstArray(line string) []instr {
 		out = append(out, instr)
 	}
 	return out
+}
+
+type pos struct {
+	Y int
+	X int
 }
 
 type instr struct {
